7d01734607505510918670053ba53dc1a220ecfd
AbhijitSarode <arsarode@hotmail.com>
2023-04-23 14:24:48 +0530

    Project completed

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
+let globalTip;
+
 function calculate(tip = 0) {
     let subTotal = Number(document.getElementById('subtotal').value)
     let peopleCount = Number(document.getElementById('people').value)
-    
-    let totalTip = subTotal * (tip / 100)
-    
-    console.log(subTotal, peopleCount, totalTip);
 
-    bill = subTotal / peopleCount
+	if(tip != 0) {
+		globalTip = tip
+	}
+    
+   	let totalTip = subTotal * (globalTip / 100)
+	let tipAmountPerPerson = totalTip / peopleCount
+    let bill = (subTotal + totalTip) / peopleCount
+
+    
+	console.log(subTotal, peopleCount, totalTip, bill);
+
+    document.getElementById('tip-per-person').innerText = `₹ ${tipAmountPerPerson.toFixed(2)}`
+	document.getElementById('total-per-person').innerText = `₹ ${bill.toFixed(2)}`
+}
+
+
+const reset = () => {
+	document.getElementById('subtotal').value = ''
+	document.getElementById('people').value = '1'
+	document.getElementById('tip-per-person').innerText = `₹ 0.00`
+	document.getElementById('total-per-person').innerText = `₹ 0.00`
+	globalTipPercent = 0
 }
